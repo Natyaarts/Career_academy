95856a0204950d4c7faaed8453312b9e973176bb
intasolutions <intasolutionpvtltd@gmail.com>
2026-02-05 16:13:02 +0530

    Configure Django settings for deployment

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-al=s2kj$h3f084f)r-a-l3-r&5r&6$j1rtl#af=5khyhmt8ajg
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -119,6 +119,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Media files
 MEDIA_URL = '/media/'
